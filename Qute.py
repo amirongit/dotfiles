642f87e5d388eebe1c0971757b9774616ec6f9fa
mr.pino <pinoq@protonmail.com>
2020-04-25 16:04:27 +0430

    qutebrowser

---
+++ Qute.py
@@ -0,0 +1,652 @@
+c.aliases = {'w': 'session-save', 'q': 'close', 'wq': 'quit --save'}
+
+c.auto_save.interval = 15000
+
+c.auto_save.session = True
+
+c.backend = 'webengine'
+
+c.colors.completion.category.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #1e1e1e, stop:1 #1e1e1e)'
+
+c.colors.completion.category.border.bottom = '#1e1e1e'
+
+c.colors.completion.category.border.top = '#1e1e1e'
+
+c.colors.completion.category.fg = '#d4d4d4'
+
+c.colors.completion.even.bg = '#1e1e1e'
+
+c.colors.completion.fg = ['#d4d4d4']
+
+c.colors.completion.item.selected.bg = '#569cd6'
+
+c.colors.completion.item.selected.border.bottom = '#1e1e1e'
+
+c.colors.completion.item.selected.border.top = '#1e1e1e'
+
+c.colors.completion.item.selected.fg = '#1e1e1e'
+
+c.colors.completion.item.selected.match.fg = '#1e1e1e'
+
+c.colors.completion.match.fg = '#c586c0'
+
+c.colors.completion.odd.bg = '#1e1e1e'
+
+c.colors.completion.scrollbar.bg = '#808080'
+
+c.colors.completion.scrollbar.fg = '#d4d4d4'
+
+c.colors.contextmenu.menu.bg = '#1e1e1e'
+
+c.colors.contextmenu.menu.fg = '#d4d4d4'
+
+c.colors.contextmenu.selected.bg = '#d4d4d4'
+
+c.colors.contextmenu.selected.fg = '#1e1e1e'
+
+c.colors.downloads.bar.bg = '#1e1e1e'
+
+c.colors.downloads.error.bg = '#f44747'
+
+c.colors.downloads.error.fg = '#d4d4d4'
+
+c.colors.downloads.start.bg = '#4ec9b0'
+
+c.colors.downloads.start.fg = '#d4d4d4'
+
+c.colors.downloads.stop.bg = '#608b4e'
+
+c.colors.downloads.stop.fg = '#d4d4d4'
+
+c.colors.downloads.system.bg = 'none'
+
+c.colors.downloads.system.fg = 'none'
+
+c.colors.hints.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 rgba(197, 134, 192, 0.8), stop:1 rgba(100, 102, 149, 0.8))'
+
+c.colors.hints.fg = '#1e1e1e'
+
+c.colors.hints.match.fg = '#569CD6'
+
+c.colors.keyhint.bg = 'rgba(30, 30, 30, 80%)'
+
+c.colors.keyhint.fg = '#d4d4d4'
+
+c.colors.keyhint.suffix.fg = '#646695'
+
+c.colors.messages.error.bg = '#F44747'
+
+c.colors.messages.error.border = '#808080'
+
+c.colors.messages.error.fg = '#1e1e1e'
+
+c.colors.messages.info.bg = '#569cd6'
+
+c.colors.messages.info.border = '#808080'
+
+c.colors.messages.info.fg = '#1e1e1e'
+
+c.colors.messages.warning.bg = '#f44747'
+
+c.colors.messages.warning.border = '#808080'
+
+c.colors.messages.warning.fg = '#1e1e1e'
+
+c.colors.prompts.bg = '#1e1e1e'
+
+c.colors.prompts.border = '1px solid gray'
+
+c.colors.prompts.fg = '#d4d4d4'
+
+c.colors.prompts.selected.bg = '#569cd6'
+
+c.colors.statusbar.caret.bg = '#646695'
+
+c.colors.statusbar.caret.fg = '#d4d4d4'
+
+c.colors.statusbar.caret.selection.bg = '#C586C0'
+
+c.colors.statusbar.caret.selection.fg = '#d4d4d4'
+
+c.colors.statusbar.command.bg = '#1e1e1e'
+
+c.colors.statusbar.command.fg = '#d4d4d4'
+
+c.colors.statusbar.command.private.bg = '#1e1e1e'
+
+c.colors.statusbar.command.private.fg = '#d4d4d4'
+
+c.colors.statusbar.insert.bg = '#608B4E'
+
+c.colors.statusbar.insert.fg = '#d4d4d4'
+
+c.colors.statusbar.normal.bg = '#1e1e1e'
+
+c.colors.statusbar.normal.fg = '#d4d4d4'
+
+c.colors.statusbar.passthrough.bg = '#646695'
+
+c.colors.statusbar.passthrough.fg = '#d4d4d4'
+
+c.colors.statusbar.private.bg = '#1e1e1e'
+
+c.colors.statusbar.private.fg = '#d4d4d4'
+
+c.colors.statusbar.progress.bg = '#d4d4d4'
+
+c.colors.statusbar.url.error.fg = '#CE9178'
+
+c.colors.statusbar.url.fg = '#d4d4d4'
+
+c.colors.statusbar.url.hover.fg = '#5A5B83'
+
+c.colors.statusbar.url.success.http.fg = '#d4d4d4'
+
+c.colors.statusbar.url.success.https.fg = '#d4d4d4'
+
+c.colors.statusbar.url.warn.fg = '#808080'
+
+c.colors.tabs.bar.bg = '#1e1e1e'
+
+c.colors.tabs.even.bg = '#1e1e1e'
+
+c.colors.tabs.even.fg = '#d4d4d4'
+
+c.colors.tabs.indicator.error = '#f44747'
+
+c.colors.tabs.indicator.start = '#1e1e1e'
+
+c.colors.tabs.indicator.stop = '#808080'
+
+c.colors.tabs.indicator.system = 'none'
+
+c.colors.tabs.odd.bg = '#1e1e1e'
+
+c.colors.tabs.odd.fg = '#d4d4d4'
+
+c.colors.tabs.pinned.even.bg = '#808080'
+
+c.colors.tabs.pinned.even.fg = '#d4d4d4'
+
+c.colors.tabs.pinned.odd.bg = '#808080'
+
+c.colors.tabs.pinned.odd.fg = '#d4d4d4'
+
+c.colors.tabs.pinned.selected.even.bg = '#808080'
+
+c.colors.tabs.pinned.selected.even.fg = '#d4d4d4'
+
+c.colors.tabs.pinned.selected.odd.bg = '#808080'
+
+c.colors.tabs.pinned.selected.odd.fg = '#d4d4d4'
+
+c.colors.tabs.selected.even.bg = '#808080'
+
+c.colors.tabs.selected.even.fg = '#d4d4d4'
+
+c.colors.tabs.selected.odd.bg = '#808080'
+
+c.colors.tabs.selected.odd.fg = '#d4d4d4'
+
+c.colors.webpage.bg = '#d4d4d4'
+
+c.colors.webpage.prefers_color_scheme_dark = True
+
+c.completion.cmd_history_max_items = 100
+
+c.completion.delay = 0
+
+c.completion.height = '30%'
+
+c.completion.min_chars = 3
+
+c.completion.open_categories = ['searchengines', 'bookmarks', 'history']
+
+c.completion.quick = True
+
+c.completion.scrollbar.padding = 0
+
+c.completion.scrollbar.width = 8
+
+c.completion.show = 'always'
+
+c.completion.shrink = True ###
+
+c.completion.timestamp_format = '%Y-%m-%d'
+
+c.completion.use_best_match = False
+
+c.completion.web_history.exclude = []
+
+c.completion.web_history.max_items = -1
+
+c.confirm_quit = ['downloads']
+
+c.content.autoplay = True
+
+c.content.cache.appcache = True
+
+c.content.cache.maximum_pages = 0
+
+c.content.cache.size = None
+
+c.content.canvas_reading = True
+
+c.content.cookies.accept = 'all'
+
+c.content.cookies.store = True
+
+c.content.default_encoding = 'utf-8'
+
+c.content.desktop_capture = 'ask'
+
+c.content.dns_prefetch = False
+
+c.content.frame_flattening = False
+
+c.content.geolocation = 'ask'
+
+c.content.headers.accept_language = 'en-US,en;q=0.9'
+
+c.content.headers.custom = {}
+
+c.content.headers.do_not_track = True
+
+c.content.headers.referer = 'same-domain'
+
+c.content.headers.user_agent = 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {qt_key}/{qt_version} {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}'
+
+c.content.host_blocking.enabled = True
+
+c.content.host_blocking.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
+
+c.content.host_blocking.whitelist = []
+
+c.content.hyperlink_auditing = False
+
+c.content.images = True
+
+c.content.javascript.alert = True
+
+c.content.javascript.can_access_clipboard = False
+
+c.content.javascript.can_close_tabs = True ###
+
+c.content.javascript.can_open_tabs_automatically = False
+
+c.content.javascript.enabled = True
+
+c.content.javascript.log = {'unknown': 'debug', 'info': 'debug', 'warning': 'debug', 'error': 'debug'}
+
+c.content.javascript.modal_dialog = False
+
+c.content.javascript.prompt = True
+
+c.content.local_content_can_access_file_urls = True
+
+c.content.local_content_can_access_remote_urls = False
+
+c.content.local_storage = True
+
+c.content.media_capture = 'ask'
+
+c.content.mouse_lock = 'ask'
+
+c.content.mute = False
+
+c.content.netrc_file = None
+
+c.content.notifications = 'ask'
+
+c.content.pdfjs = False
+
+c.content.persistent_storage = 'ask'
+
+c.content.plugins = False
+
+c.content.print_element_backgrounds = True
+
+c.content.private_browsing = False
+
+c.content.proxy = 'socks://127.0.0.1:9050'
+
+c.content.proxy_dns_requests = True
+
+c.content.register_protocol_handler = 'ask'
+
+c.content.site_specific_quirks = True
+
+c.content.ssl_strict = 'ask'
+
+c.content.user_stylesheets = []
+
+c.content.webgl = True
+
+c.content.webrtc_ip_handling_policy = 'all-interfaces'
+
+c.content.windowed_fullscreen = True ###
+
+c.content.xss_auditing = False
+
+c.downloads.location.directory = '/home/amir/Downloads'
+
+c.downloads.location.prompt = False
+
+c.downloads.location.remember = True
+
+c.downloads.location.suggestion = 'both'
+
+c.downloads.open_dispatcher = None
+
+c.downloads.position = 'bottom'
+
+c.downloads.remove_finished = -1
+
+c.editor.encoding = 'utf-8'
+
+c.fonts.completion.category = '8pt mononoki'
+
+c.fonts.completion.entry = '8pt mononoki'
+
+c.fonts.contextmenu = '8pt Estedad'
+
+c.fonts.debug_console = '12pt Cascadia Code'
+
+c.fonts.default_family = ['8pt Estedad', '8pt Tanha']
+
+c.fonts.default_size = '10pt'
+
+c.fonts.downloads = '10pt Estedad'
+
+c.fonts.hints = '8pt mononoki'
+
+c.fonts.keyhint = '8pt mononoki'
+
+c.fonts.messages.error = '10pt Cascadia Code'
+
+c.fonts.messages.info = '10pt Estedad'
+
+c.fonts.messages.warning = '10pt Cascadia Code'
+
+c.fonts.prompts = '8pt mononoki'
+
+c.fonts.statusbar = '8pt mononoki'
+
+c.fonts.tabs = '8pt Blogger Sans'
+
+c.fonts.web.family.cursive = 'Blogger Sans'
+
+c.fonts.web.family.fantasy = 'Estedad'
+
+c.fonts.web.family.fixed = 'Cascadia Code'
+
+c.fonts.web.family.sans_serif = 'Tanha'
+
+c.fonts.web.family.serif = 'Estedad'
+
+c.fonts.web.family.standard = 'Tanha'
+
+c.fonts.web.size.default = 12
+
+c.fonts.web.size.default_fixed = 12
+
+c.fonts.web.size.minimum = 0
+
+c.fonts.web.size.minimum_logical = 6
+
+c.hints.auto_follow = 'unique-match'
+
+c.hints.auto_follow_timeout = 0
+
+c.hints.border = '1px solid gray'
+
+c.hints.chars = 'asdfghjkl'
+
+c.hints.dictionary = '/usr/share/dict/words'
+
+c.hints.find_implementation = 'python'
+
+c.hints.hide_unmatched_rapid_hints = True
+
+c.hints.leave_on_load = True
+
+c.hints.min_chars = 1
+
+c.hints.mode = 'letter'
+
+c.hints.next_regexes = ['\\bnext\\b', '\\bmore\\b', '\\bnewer\\b', '\\b[>→≫]\\b', '\\b(>>|»)\\b', '\\bcontinue\\b']
+
+c.hints.prev_regexes = ['\\bprev(ious)?\\b', '\\bback\\b', '\\bolder\\b', '\\b[<←≪]\\b', '\\b(<<|«)\\b']
+
+c.hints.scatter = True
+
+c.hints.selectors = {'all': ['a', 'area', 'textarea', 'select', 'input:not([type="hidden"])', 'button', 'frame', 'iframe', 'img', 'link', 'summary', '[onclick]', '[onmousedown]', '[role="link"]', '[role="option"]', '[role="button"]', '[ng-click]', '[ngClick]', '[data-ng-click]', '[x-ng-click]', '[tabindex]'], 'links': ['a[href]', 'area[href]', 'link[href]', '[role="link"][href]'], 'images': ['img'], 'media': ['audio', 'img', 'video'], 'url': ['[src]', '[href]'], 'inputs': ['input[type="text"]', 'input[type="date"]', 'input[type="datetime-local"]', 'input[type="email"]', 'input[type="month"]', 'input[type="number"]', 'input[type="password"]', 'input[type="search"]', 'input[type="tel"]', 'input[type="time"]', 'input[type="url"]', 'input[type="week"]', 'input:not([type])', 'textarea']}
+
+c.hints.uppercase = False
+
+c.history_gap_interval = 30
+
+c.input.escape_quits_reporter = True
+
+c.input.forward_unbound_keys = 'auto'
+
+c.input.insert_mode.auto_enter = True
+
+c.input.insert_mode.auto_leave = True
+
+c.input.insert_mode.auto_load = True
+
+c.input.insert_mode.leave_on_load = True
+
+c.input.insert_mode.plugins = False
+
+c.input.links_included_in_focus_chain = True
+
+c.input.partial_timeout = 5000
+
+c.input.rocker_gestures = True
+
+c.input.spatial_navigation = False
+
+c.keyhint.blacklist = []
+
+c.keyhint.delay = 500
+
+c.keyhint.radius = 6
+
+c.messages.timeout = 2000
+
+c.new_instance_open_target = 'tab'
+
+c.new_instance_open_target_window = 'last-focused'
+
+c.prompt.filebrowser = True
+
+c.prompt.radius = 8
+
+c.qt.args = []
+
+c.qt.force_platform = None
+
+c.qt.force_platformtheme = None
+
+#   - software-opengl: Tell LibGL to use a software implementation of GL (`LIBGL_ALWAYS_SOFTWARE` / `QT_XCB_FORCE_SOFTWARE_OPENGL`)
+#   - qt-quick: Tell Qt Quick to use a software renderer instead of OpenGL. (`QT_QUICK_BACKEND=software`)
+#   - chromium: Tell Chromium to disable GPU support and use Skia software rendering instead. (`--disable-gpu`)
+#   - none: Don't force software rendering.
+c.qt.force_software_rendering = 'none'
+
+c.qt.highdpi = False
+
+c.qt.low_end_device_mode = 'auto'
+
+c.qt.process_model = 'process-per-site-instance'
+
+c.scrolling.bar = 'when-searching'
+
+c.scrolling.smooth = False
+
+c.search.ignore_case = 'smart'
+
+c.search.incremental = True
+
+c.session.default_name = None
+
+c.session.lazy_restore = False
+
+c.spellcheck.languages = []
+
+c.statusbar.hide = False
+
+c.statusbar.padding = {'top': 0, 'bottom': 0, 'left': 0, 'right': 0}
+
+c.statusbar.position = 'bottom'
+
+#   - url: Current page URL.
+#   - scroll: Percentage of the current page position like `10%`.
+#   - scroll_raw: Raw percentage of the current page position like `10`.
+#   - history: Display an arrow when possible to go back/forward in history.
+#   - tabs: Current active tab, e.g. `2`.
+#   - keypress: Display pressed keys when composing a vi command.
+#   - progress: Progress bar for the current page loading.
+c.statusbar.widgets = ['keypress', 'url', 'history', 'tabs', 'progress']
+
+c.tabs.background = True
+
+c.tabs.close_mouse_button = 'none'
+
+c.tabs.close_mouse_button_on_bar = 'new-tab'
+
+c.tabs.favicons.scale = 1.0
+
+c.tabs.favicons.show = 'never'
+
+c.tabs.focus_stack_size = 10
+
+c.tabs.indicator.padding = {'top': 0, 'bottom': 0, 'left': 0, 'right': 4}
+
+c.tabs.indicator.width = 2
+
+c.tabs.last_close = 'default-page'
+
+c.tabs.max_width = -1
+
+c.tabs.min_width = -1
+
+c.tabs.mode_on_change = 'normal'
+
+c.tabs.mousewheel_switching = False
+
+c.tabs.new_position.related = 'next'
+
+c.tabs.new_position.stacking = True
+
+c.tabs.new_position.unrelated = 'last'
+
+c.tabs.padding = {'top': 0, 'bottom': 0, 'left': 0, 'right': 0}
+
+c.tabs.pinned.frozen = True
+
+c.tabs.pinned.shrink = True
+
+c.tabs.position = 'top'
+
+c.tabs.select_on_remove = 'last-used'
+
+#   - always: Always show the tab bar.
+#   - never: Always hide the tab bar.
+#   - multiple: Hide the tab bar if only one tab is open.
+#   - switching: Show the tab bar when switching tabs.
+c.tabs.show = 'always'
+
+c.tabs.show_switching_delay = 1500
+
+c.tabs.tabs_are_windows = False
+
+c.tabs.title.alignment = 'left'
+
+c.tabs.title.format = '{audio}{index}: {current_title}'
+
+c.tabs.title.format_pinned = '{index}'
+
+c.tabs.tooltips = False
+
+c.tabs.undo_stack_size = 30
+
+c.tabs.width = '20%'
+
+c.tabs.wrap = True
+
+c.url.auto_search = 'naive'
+
+c.url.default_page = 'https://start.duckduckgo.com/'
+
+c.url.incdec_segments = ['path', 'query']
+
+c.url.open_base_url = False
+
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'g': 'https://duckduckgo.com/?q=!g%20{}'}
+
+c.url.start_pages = ['https://start.duckduckgo.com']
+
+c.url.yank_ignored_parameters = ['ref', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content']
+
+c.window.hide_decoration = False
+
+c.window.title_format = '{perc}{current_title}{title_sep}qutebrowser'
+
+c.zoom.default = '100%'
+
+c.zoom.levels = ['25%', '33%', '50%', '67%', '75%', '90%', '100%', '110%', '125%', '150%', '175%', '200%', '250%', '300%', '400%', '500%']
+
+c.zoom.mouse_divider = 512
+
+c.zoom.text_only = False
+
+config.bind('+', 'zoom-in')
+config.bind('-', 'zoom-out')
+config.bind('.', 'repeat-command')
+config.bind('/', 'set-cmd-text /')
+config.bind(':', 'set-cmd-text :')
+config.bind('I', 'hint images tab')
+config.bind('D', 'hint links download')
+config.bind('O', 'hint all tab-fg')
+config.bind('<Alt-1>', 'tab-focus 1')
+config.bind('<Alt-2>', 'tab-focus 2')
+config.bind('<Alt-3>', 'tab-focus 3')
+config.bind('<Alt-4>', 'tab-focus 4')
+config.bind('<Alt-5>', 'tab-focus 5')
+config.bind('<Alt-6>', 'tab-focus 6')
+config.bind('<Alt-7>', 'tab-focus 7')
+config.bind('<Alt-8>', 'tab-focus 8')
+config.bind('<Alt-9>', 'tab-focus -1')
+config.bind('<Alt-m>', 'tab-mute')
+config.bind('R', 'reload -f')
+config.bind('P', 'tab-pin')
+config.bind('S', 'stop')
+config.bind('<Escape>', 'clear-keychain ;; search ;; fullscreen --leave')
+config.bind('?', 'set-cmd-text ?')
+config.bind('Q', 'tab-close')
+config.bind('J', 'back')
+config.bind('L', 'tab-next')
+config.bind('H', 'tab-prev')
+config.bind('K', 'forward')
+config.bind('M', 'bookmark-add')
+config.bind('h', 'scroll left')
+config.bind('i', 'enter-mode insert')
+config.bind('j', 'scroll down')
+config.bind('k', 'scroll up')
+config.bind('l', 'scroll right')
+config.bind('o', 'set-cmd-text -s :open')
+config.bind('m', 'record-macro')
+config.bind('u', 'undo')
+config.bind('<Escape>', 'leave-mode', mode='hint')
+config.bind('<Escape>', 'leave-mode', mode='insert')
+
+
+config.bind('<Down>', 'prompt-item-focus next', mode='prompt')
+config.bind('<Escape>', 'leave-mode', mode='prompt')
+config.bind('<Return>', 'prompt-accept', mode='prompt')
+config.bind('<Tab>', 'prompt-item-focus next', mode='prompt')
+config.bind('<Up>', 'prompt-item-focus prev', mode='prompt')
+
