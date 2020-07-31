4d1fcafb5db37da246a357108be00bd7523a9f18
mr.pino <pinoq@protonmail.com>
2020-08-01 01:58:55 +0430

    fixed context menu bug in qute

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,7 +6,189 @@ c.auto_save.session = True
 
 c.backend = 'webengine'
 
-c.colors.prompts.border = '1px solid gray'
+c.colors.completion.category.bg = base00
+
+c.colors.completion.category.border.bottom = base00
+
+c.colors.completion.category.border.top = base00
+
+c.colors.completion.category.fg = base0D
+
+c.colors.completion.even.bg = base00
+
+c.colors.completion.fg = base05
+
+c.colors.completion.item.selected.bg = base0D
+
+c.colors.completion.item.selected.border.bottom = base0D
+
+c.colors.completion.item.selected.border.top = base0D
+
+c.colors.completion.item.selected.fg = base00
+
+c.colors.completion.item.selected.match.fg = base00
+
+c.colors.completion.match.fg = base09
+
+c.colors.completion.odd.bg = base00
+
+c.colors.completion.scrollbar.bg = base00
+
+c.colors.completion.scrollbar.fg = base05
+
+c.colors.contextmenu.menu.bg = base00
+
+c.colors.contextmenu.menu.fg = base05
+
+c.colors.contextmenu.selected.bg = base0D
+
+c.colors.contextmenu.selected.fg = base00
+
+c.colors.downloads.bar.bg = base00
+
+c.colors.downloads.error.bg = base0E
+
+c.colors.downloads.error.fg = base08
+
+c.colors.downloads.start.bg = base0D
+
+c.colors.downloads.start.fg = base00
+
+c.colors.downloads.stop.bg = base0C
+
+c.colors.downloads.stop.fg = base00
+
+c.colors.downloads.system.bg = 'none'
+
+c.colors.downloads.system.fg = 'none'
+
+c.colors.hints.bg = base0A
+
+c.colors.hints.fg = base00
+
+c.colors.hints.match.fg = base05
+
+c.colors.keyhint.bg = base00
+
+c.colors.keyhint.fg = base05
+
+c.colors.keyhint.suffix.fg = base05
+
+c.colors.messages.error.bg = base08
+
+c.colors.messages.error.border = base08
+
+c.colors.messages.error.fg = base00
+
+c.colors.messages.info.bg = base00
+
+c.colors.messages.info.border = base00
+
+c.colors.messages.info.fg = base05
+
+c.colors.messages.warning.bg = base0E
+
+c.colors.messages.warning.border = base0E
+
+c.colors.messages.warning.fg = base00
+
+c.colors.prompts.bg = base00
+
+c.colors.prompts.border = base00
+
+c.colors.prompts.fg = base05
+
+c.colors.prompts.selected.bg = base0A
+
+c.colors.statusbar.caret.bg = base00
+
+c.colors.statusbar.caret.fg = base0D
+
+c.colors.statusbar.caret.selection.bg = base00
+
+c.colors.statusbar.caret.selection.fg = base0D
+
+c.colors.statusbar.command.bg = base01
+
+c.colors.statusbar.command.fg = base04
+
+c.colors.statusbar.command.private.bg = base01
+
+c.colors.statusbar.command.private.fg = base0E
+
+c.colors.statusbar.insert.bg = base00
+
+c.colors.statusbar.insert.fg = base0C
+
+c.colors.statusbar.normal.bg = base00
+
+c.colors.statusbar.normal.fg = base05
+
+c.colors.statusbar.passthrough.bg = base00
+
+c.colors.statusbar.passthrough.fg = base0A
+
+c.colors.statusbar.private.bg = base00
+
+c.colors.statusbar.private.fg = base0E
+
+c.colors.statusbar.progress.bg = base0D
+
+c.colors.statusbar.url.error.fg = base08
+
+c.colors.statusbar.url.fg = base05
+
+c.colors.statusbar.url.hover.fg = base09
+
+c.colors.statusbar.url.success.http.fg = base0B
+
+c.colors.statusbar.url.success.https.fg = base0B
+
+c.colors.statusbar.url.warn.fg = base0E
+
+c.colors.tabs.bar.bg = base00
+
+c.colors.tabs.even.bg = base00
+
+c.colors.tabs.even.fg = base05
+
+c.colors.tabs.indicator.error = base08
+
+c.colors.tabs.indicator.start = base0D
+
+c.colors.tabs.indicator.stop = base0C
+
+c.colors.tabs.indicator.system = 'none'
+
+c.colors.tabs.odd.bg = base00
+
+c.colors.tabs.odd.fg = base05
+
+c.colors.tabs.pinned.even.bg = base0B
+
+c.colors.tabs.pinned.even.fg = base00
+
+c.colors.tabs.pinned.odd.bg = base0B
+
+c.colors.tabs.pinned.odd.fg = base00
+
+c.colors.tabs.pinned.selected.even.bg = base0D
+
+c.colors.tabs.pinned.selected.even.fg = base00
+
+c.colors.tabs.pinned.selected.odd.bg = base0D
+
+c.colors.tabs.pinned.selected.odd.fg = base00
+
+c.colors.tabs.selected.even.bg = base0D
+
+c.colors.tabs.selected.even.fg = base00
+
+c.colors.tabs.selected.odd.bg = base0D
+
+c.colors.tabs.selected.odd.fg = base00
+
+c.colors.webpage.bg = base00
 
 c.colors.webpage.prefers_color_scheme_dark = True
 
@@ -28,7 +210,7 @@ c.completion.scrollbar.width = 8
 
 c.completion.show = 'always'
 
-c.completion.shrink = True
+c.completion.shrink = True ###
 
 c.completion.timestamp_format = '%Y-%m-%d'
 
@@ -88,7 +270,7 @@ c.content.javascript.alert = True
 
 c.content.javascript.can_access_clipboard = False
 
-c.content.javascript.can_close_tabs = True
+c.content.javascript.can_close_tabs = True ###
 
 c.content.javascript.can_open_tabs_automatically = False
 
@@ -126,7 +308,7 @@ c.content.print_element_backgrounds = True
 
 c.content.private_browsing = False
 
-c.content.proxy = 'system'
+c.content.proxy = 'socks://127.0.0.1:9050'
 
 c.content.proxy_dns_requests = True
 
@@ -142,7 +324,7 @@ c.content.webgl = True
 
 c.content.webrtc_ip_handling_policy = 'all-interfaces'
 
-c.content.fullscreen.window = True
+c.content.fullscreen.window = True ###
 
 c.content.xss_auditing = False
 
@@ -162,50 +344,46 @@ c.downloads.remove_finished = -1
 
 c.editor.encoding = 'utf-8'
 
-c.fonts.completion.category = '10pt inconsolata SemiBold'
+c.fonts.completion.category = '8pt mononoki'
 
-c.fonts.completion.entry = '10pt inconsolata SemiBold'
+c.fonts.completion.entry = '8pt mononoki'
 
-c.fonts.contextmenu = '8pt Cantarell'
+c.fonts.contextmenu = '8pt Estedad'
 
-c.fonts.debug_console = '10pt inconsolata SemiBold'
+c.fonts.debug_console = '12pt Cascadia Code'
 
-c.fonts.default_family = ['8pt Cantarell', '8pt Blogger Sans']
+c.fonts.default_family = ['8pt Estedad', '8pt Tanha']
 
 c.fonts.default_size = '10pt'
 
-c.fonts.downloads = '8pt Cantarell'
+c.fonts.downloads = '10pt Estedad'
 
-c.fonts.hints = '10pt inconsolata SemiBold'
+c.fonts.hints = '8pt mononoki'
 
-c.fonts.keyhint = '10pt inconsolata SemiBold'
+c.fonts.keyhint = '8pt mononoki'
 
-c.fonts.messages.error = '10pt inconsolata SemiBold'
+c.fonts.messages.error = '10pt Cascadia Code'
 
-c.fonts.messages.info = '10pt inconsolata SemiBold'
+c.fonts.messages.info = '10pt Estedad'
 
-c.fonts.messages.warning = '10pt inconsolata SemiBold'
+c.fonts.messages.warning = '10pt Cascadia Code'
 
-c.fonts.prompts = '10pt inconsolata SemiBold'
+c.fonts.prompts = '8pt mononoki'
 
-c.fonts.statusbar = '10pt inconsolata SemiBold'
-
-c.fonts.tabs.selected = '10pt inconsolata SemiBold'
-
-c.fonts.tabs.unselected = '10pt inconsolata SemiBold'
+c.fonts.statusbar = '8pt mononoki'
 
 
 c.fonts.web.family.cursive = 'Blogger Sans'
 
-c.fonts.web.family.fantasy = 'Cantarell'
+c.fonts.web.family.fantasy = 'Estedad'
 
-c.fonts.web.family.fixed = 'inconsolata SemiBold'
+c.fonts.web.family.fixed = 'Cascadia Code'
 
-c.fonts.web.family.sans_serif = 'Blogger Sans'
+c.fonts.web.family.sans_serif = 'Tanha'
 
-c.fonts.web.family.serif = 'Cantarell'
+c.fonts.web.family.serif = 'Estedad'
 
-c.fonts.web.family.standard = 'Blogger Sans'
+c.fonts.web.family.standard = 'Tanha'
 
 c.fonts.web.size.default = 12
 
@@ -265,7 +443,6 @@ c.input.links_included_in_focus_chain = True
 
 c.input.partial_timeout = 5000
 
-c.input.mouse.rocker_gestures = True
 
 c.input.spatial_navigation = False
 
@@ -295,7 +472,7 @@ c.qt.force_platformtheme = None
 #   - qt-quick: Tell Qt Quick to use a software renderer instead of OpenGL. (`QT_QUICK_BACKEND=software`)
 #   - chromium: Tell Chromium to disable GPU support and use Skia software rendering instead. (`--disable-gpu`)
 #   - none: Don't force software rendering.
-c.qt.force_software_rendering = 'qt-quick'
+c.qt.force_software_rendering = 'none'
 
 c.qt.highdpi = False
 
@@ -303,7 +480,7 @@ c.qt.low_end_device_mode = 'auto'
 
 c.qt.process_model = 'process-per-site-instance'
 
-c.scrolling.bar = 'always'
+c.scrolling.bar = 'when-searching'
 
 c.scrolling.smooth = False
 
@@ -377,7 +554,7 @@ c.tabs.select_on_remove = 'last-used'
 #   - never: Always hide the tab bar.
 #   - multiple: Hide the tab bar if only one tab is open.
 #   - switching: Show the tab bar when switching tabs.
-c.tabs.show = 'multiple'
+c.tabs.show = 'always'
 
 c.tabs.show_switching_delay = 1500
 
@@ -399,13 +576,13 @@ c.tabs.wrap = True
 
 c.url.auto_search = 'naive'
 
-c.url.default_page = 'https://start.duckduckgo.com'
+c.url.default_page = 'https://start.duckduckgo.com/'
 
 c.url.incdec_segments = ['path', 'query']
 
 c.url.open_base_url = False
 
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'g': 'https://duckduckgo.com/?q=!g%20{}'}
 
 c.url.start_pages = ['https://start.duckduckgo.com']
 
@@ -422,292 +599,6 @@ c.zoom.levels = ['25%', '33%', '50%', '67%', '75%', '90%', '100%', '110%', '125%
 c.zoom.mouse_divider = 512
 
 c.zoom.text_only = False
-
-base00 = "#181818"
-base01 = "#282828"
-base02 = "#383838"
-base03 = "#585858"
-base04 = "#b8b8b8"
-base05 = "#d8d8d8"
-base06 = "#e8e8e8"
-base07 = "#f8f8f8"
-base08 = "#ab4642"
-base09 = "#dc9656"
-base0A = "#f7ca88"
-base0B = "#a1b56c"
-base0C = "#86c1b9"
-base0D = "#7cafc2"
-base0E = "#ba8baf"
-base0F = "#a16946"
-
-# Text color of the completion widget. May be a single color to use for
-# all columns or a list of three colors, one for each column.
-c.colors.completion.fg = base05
-
-# Background color of the completion widget for odd rows.
-c.colors.completion.odd.bg = base03
-
-# Background color of the completion widget for even rows.
-c.colors.completion.even.bg = base00
-
-# Foreground color of completion widget category headers.
-c.colors.completion.category.fg = base0A
-
-# Background color of the completion widget category headers.
-c.colors.completion.category.bg = base00
-
-# Top border color of the completion widget category headers.
-c.colors.completion.category.border.top = base00
-
-# Bottom border color of the completion widget category headers.
-c.colors.completion.category.border.bottom = base00
-
-# Foreground color of the selected completion item.
-c.colors.completion.item.selected.fg = base01
-
-# Background color of the selected completion item.
-c.colors.completion.item.selected.bg = base0A
-
-# Top border color of the selected completion item.
-c.colors.completion.item.selected.border.top = base0A
-
-# Bottom border color of the selected completion item.
-c.colors.completion.item.selected.border.bottom = base0A
-
-# Foreground color of the matched text in the selected completion item.
-c.colors.completion.item.selected.match.fg = base08
-
-# Foreground color of the matched text in the completion.
-c.colors.completion.match.fg = base0B
-
-# Color of the scrollbar handle in the completion view.
-c.colors.completion.scrollbar.fg = base05
-
-# Color of the scrollbar in the completion view.
-c.colors.completion.scrollbar.bg = base00
-
-# Background color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.bg = base00
-
-# Foreground color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.fg =  base05
-
-# Background color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.bg = base0A
-
-#Foreground color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.fg = base01
-
-# Background color for the download bar.
-c.colors.downloads.bar.bg = base00
-
-# Color gradient start for download text.
-c.colors.downloads.start.fg = base00
-
-# Color gradient start for download backgrounds.
-c.colors.downloads.start.bg = base0D
-
-# Color gradient end for download text.
-c.colors.downloads.stop.fg = base00
-
-# Color gradient stop for download backgrounds.
-c.colors.downloads.stop.bg = base0C
-
-# Foreground color for downloads with errors.
-c.colors.downloads.error.fg = base08
-
-# Font color for hints.
-c.colors.hints.fg = base00
-
-# Background color for hints. Note that you can use a `rgba(...)` value
-# for transparency.
-c.colors.hints.bg = base0A
-
-# Font color for the matched part of hints.
-c.colors.hints.match.fg = base05
-
-# Text color for the keyhint widget.
-c.colors.keyhint.fg = base05
-
-# Highlight color for keys to complete the current keychain.
-c.colors.keyhint.suffix.fg = base05
-
-# Background color of the keyhint widget.
-c.colors.keyhint.bg = base00
-
-# Foreground color of an error message.
-c.colors.messages.error.fg = base00
-
-# Background color of an error message.
-c.colors.messages.error.bg = base08
-
-# Border color of an error message.
-c.colors.messages.error.border = base08
-
-# Foreground color of a warning message.
-c.colors.messages.warning.fg = base00
-
-# Background color of a warning message.
-c.colors.messages.warning.bg = base0E
-
-# Border color of a warning message.
-c.colors.messages.warning.border = base0E
-
-# Foreground color of an info message.
-c.colors.messages.info.fg = base05
-
-# Background color of an info message.
-c.colors.messages.info.bg = base00
-
-# Border color of an info message.
-c.colors.messages.info.border = base00
-
-# Foreground color for prompts.
-c.colors.prompts.fg = base05
-
-# Border used around UI elements in prompts.
-c.colors.prompts.border = base00
-
-# Background color for prompts.
-c.colors.prompts.bg = base00
-
-# Background color for the selected item in filename prompts.
-c.colors.prompts.selected.bg = base0A
-
-# Foreground color of the statusbar.
-c.colors.statusbar.normal.fg = base0B
-
-# Background color of the statusbar.
-c.colors.statusbar.normal.bg = base00
-
-# Foreground color of the statusbar in insert mode.
-c.colors.statusbar.insert.fg = base00
-
-# Background color of the statusbar in insert mode.
-c.colors.statusbar.insert.bg = base0D
-
-# Foreground color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.fg = base00
-
-# Background color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.bg = base0C
-
-# Foreground color of the statusbar in private browsing mode.
-c.colors.statusbar.private.fg = base00
-
-# Background color of the statusbar in private browsing mode.
-c.colors.statusbar.private.bg = base03
-
-# Foreground color of the statusbar in command mode.
-c.colors.statusbar.command.fg = base05
-
-# Background color of the statusbar in command mode.
-c.colors.statusbar.command.bg = base00
-
-# Foreground color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.fg = base05
-
-# Background color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.bg = base00
-
-# Foreground color of the statusbar in caret mode.
-c.colors.statusbar.caret.fg = base00
-
-# Background color of the statusbar in caret mode.
-c.colors.statusbar.caret.bg = base0E
-
-# Foreground color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.fg = base00
-
-# Background color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.bg = base0D
-
-# Background color of the progress bar.
-c.colors.statusbar.progress.bg = base0D
-
-# Default foreground color of the URL in the statusbar.
-c.colors.statusbar.url.fg = base05
-
-# Foreground color of the URL in the statusbar on error.
-c.colors.statusbar.url.error.fg = base08
-
-# Foreground color of the URL in the statusbar for hovered links.
-c.colors.statusbar.url.hover.fg = base05
-
-# Foreground color of the URL in the statusbar on successful load
-# (http).
-c.colors.statusbar.url.success.http.fg = base0C
-
-# Foreground color of the URL in the statusbar on successful load
-# (https).
-c.colors.statusbar.url.success.https.fg = base0B
-
-# Foreground color of the URL in the statusbar when there's a warning.
-c.colors.statusbar.url.warn.fg = base0E
-
-# Background color of the tab bar.
-c.colors.tabs.bar.bg = base00
-
-# Color gradient start for the tab indicator.
-c.colors.tabs.indicator.start = base0D
-
-# Color gradient end for the tab indicator.
-c.colors.tabs.indicator.stop = base0C
-
-# Color for the tab indicator on errors.
-c.colors.tabs.indicator.error = base08
-
-# Foreground color of unselected odd tabs.
-c.colors.tabs.odd.fg = base05
-
-# Background color of unselected odd tabs.
-c.colors.tabs.odd.bg = base03
-
-# Foreground color of unselected even tabs.
-c.colors.tabs.even.fg = base05
-
-# Background color of unselected even tabs.
-c.colors.tabs.even.bg = base00
-
-# Background color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.bg = base0C
-
-# Foreground color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.fg = base07
-
-# Background color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.bg = base0B
-
-# Foreground color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.fg = base07
-
-# Background color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.bg = base05
-
-# Foreground color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.fg = base00
-
-# Background color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.bg = base05
-
-# Foreground color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.fg = base0E
-
-# Foreground color of selected odd tabs.
-c.colors.tabs.selected.odd.fg = base00
-
-# Background color of selected odd tabs.
-c.colors.tabs.selected.odd.bg = base05
-
-# Foreground color of selected even tabs.
-c.colors.tabs.selected.even.fg = base00
-
-# Background color of selected even tabs.
-c.colors.tabs.selected.even.bg = base05
-
-# Background color for webpages if unset (or empty to use the theme's
-# color).
-# c.colors.webpage.bg = base00
 
 
 config.bind('+', 'zoom-in')
