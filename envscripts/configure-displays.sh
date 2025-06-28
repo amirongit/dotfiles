b87716983b49e0f5ccba067c045282aeb6b57dec
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-06-28 08:42:20 +0330

    father forgive me for I vibe coded this thing

--- envscripts/configure-displays.sh
+++ envscripts/configure-displays.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-lid_state=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
+lid_state=$(awk '{print $2}' /proc/acpi/button/lid/LID/state )
+number_of_monitors=$(xrandr --listactivemonitors | grep Monitors | cut -d ' ' -f 2)
 
 if [ "$lid_state" = "open" ]; then
-    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off
-elif [ "$lid_state" = "closed" ]; then
+    if [ "$number_of_monitors " -eq 2 ]; then
+        xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off
+    else
+        xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off
+    fi
+elif [ "$lid_state" = "closed" ] && [ "$number_of_monitors " -eq 2 ]; then
     xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off
 else
     exit 1
